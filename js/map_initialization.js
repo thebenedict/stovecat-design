2c54f072c58161255128a4e3c1189424f2ba21f1
thebenedict <thebenedict@gmail.com>
2015-04-14 17:35:50 +0200

    fuel show

--- js/map_initialization.js
+++ js/map_initialization.js
@@ -1,16 +1,29 @@
-$(document).ready(function() {
-	var map = new Datamap({element: document.getElementById("fuel-deploy-map"),
-		geographyConfig: {
-    		popupOnHover: false,
-    		highlightOnHover: false
-  		},
-  		fills: {
-  			defaultFill: "#ddd",
-  			deployed: "#e56120"
-  		},
-  		data: {
-  			KEN: { fillKey: "deployed" },
-  			TZA: { fillKey: "deployed" }
-  		}
-  	});
+
+$(window).load(function() {
+  createMap();
 });
+
+$(window).on('resize', function() {
+  clearInterval(loop);
+  var loop = setInterval(createMap, 30);
+});
+
+var createMap = function() {
+  $('#fuel-deploy-map').empty();
+  var map = new Datamap({
+    element: document.getElementById("fuel-deploy-map"),
+    geographyConfig: {
+        popupOnHover: false,
+        highlightOnHover: false
+      },
+      fills: {
+        defaultFill: "#ddd",
+        deployed: "#e56120"
+      },
+      data: {
+        KEN: { fillKey: "deployed" },
+        TZA: { fillKey: "deployed" }
+      }
+    });
+}
+
